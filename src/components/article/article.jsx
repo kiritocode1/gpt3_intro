4d02177206033bc2b42dc042df805cf4ba287e88
kiritocode1 <kathawalearyan9@gmail.com>
2022-03-29 00:43:03 +0530

    initial commit

---
+++ src/components/article/article.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./article.css";
+const article = () => {
+  return (
+    <div>article</div>
+  )
+}
+
+export default article;
---
+++ src/components/brand/brand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./brand.css";
+const brand = () => {
+  return (
+    <div>brand</div>
+  )
+}
+
+export default brand
