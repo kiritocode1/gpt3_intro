4b5cccc00d5994d21c42361d1498265534d9cc22
kiritocode1 <kathawalearyan9@gmail.com>
2022-04-08 12:51:07 +0530

    header-ui complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-
-
+import React from "react";
+import {CssBaseline, Grid} from "@material-ui/core"
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
@@ -19,9 +19,18 @@ import Map from "./components/Map/Map";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="">hello world</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map/>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
