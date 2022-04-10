ca2b330370a2c67238a491e119317946b50db077
kiritocode1 <kathawalearyan9@gmail.com>
2022-04-10 19:34:13 +0530

    completed this basic step of adding credentials

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import GoogleMapReact from "google-map-react";
 import { paper, Typography, useMediaQuery } from "@material-ui/core";
 //? paper is just div+bg color 
-import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 import useStyles from "./styles.js"
 
 const Map = () => {
+    const classes = useStyles();
+    // ?turns styles from the styles.js
+    const isMobile = useMediaQuery('(min-width:600px)');
+    //? isMobile variable will turn false when the screen-width is larger than 600 px ;
+    const coordinates={lat:0, lng:0}
     return (
-        <div>Map</div>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact bootstrapURLKeys={{ key: 'AIzaSyAsuP38ZcJiZfB-dIicFOVOvsycSvrPwgk' }} defaultCenter={coordinates} center={coordinates} defaultZoom={14} margin={[50,50,50,50]} options={''} onChange={''} onChildClick={''}>
+                {/* to get the key we goto google developer's console to interact 
+                get the key here ->  https://console.cloud.google.com/projectcreate 
+                log in , and  make a new project{TravelAdvisor} ,
+                goto api lib 
+                get the google javascript maps 
+                create credentials from credentials 
+                */}
+            </GoogleMapReact>
+        </div> 
     )
 };
 
